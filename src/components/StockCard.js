c907c74dc549eea77c54785977eb37fb0a03e17b
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-14 09:59:53 -0400

    got the stock cards to toggle

--- src/components/StockCard.js
+++ src/components/StockCard.js
@@ -3,13 +3,25 @@ import React, { Component } from 'react';
 export default class StockCard extends Component {
 
 	state = {
-		clicked: false
+		clicked: false,
+		profile: {}
 	}
 
+	handleClick = () => {
+		this.setState({clicked: !this.state.clicked});
+		fetch(`https://financialmodelingprep.com/api/v3/company/profile/${this.props.stock.symbol}`)
+		.then(res => res.json())
+		.then(stockData => this.setState({profile: stockData.profile}))
+	}
+
+
+
 	render() {
+		// console.log("clicked", this.state.clicked);
+		// console.log("profile", this.state.profile)
 		return(
 			<div className="box" onClick={() => this.handleClick()}>
-				{this.props.stock.name}   ${this.props.stock.price}
+				{Object.keys(this.state.profile).length > 0 && this.state.clicked ? <img src={this.state.profile.image} /> : `${this.props.stock.name} $${this.props.stock.price}`}
 			</div>
 		)
 	}
