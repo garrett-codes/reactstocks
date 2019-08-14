0e1995b705a2d989b63edc7aae63fb0a5a9d337c
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-14 10:31:26 -0400

    added align

--- src/components/StockCard.js
+++ src/components/StockCard.js
@@ -14,6 +14,13 @@ export default class StockCard extends Component {
 		.then(stockData => this.setState({profile: stockData.profile}))
 	}
 
+	renderProfile = () => {
+		return (<div>
+			
+		</div>
+		);
+	}
+
 
 
 	render() {
@@ -21,7 +28,19 @@ export default class StockCard extends Component {
 		// console.log("profile", this.state.profile)
 		return(
 			<div className="box" onClick={() => this.handleClick()}>
-				{Object.keys(this.state.profile).length > 0 && this.state.clicked ? <img src={this.state.profile.image} /> : `${this.props.stock.name} $${this.props.stock.price}`}
+				{
+					Object.keys(this.state.profile).length > 0 && this.state.clicked ? 
+					<div>
+						<img height={"50%"} width={"50%"} src={this.state.profile.image} />
+						<div>
+							<br /> Percent Change: {this.state.profile.changesPercentage}
+							<br /> Description: {this.state.profile.description}
+							<br /> Exchange: {this.state.profile.exchange}
+							<br /> Sector: {this.state.profile.sector} 
+						</div>
+					</div>
+					: 
+						`${this.props.stock.name} $${this.props.stock.price}`}
 			</div>
 		)
 	}
